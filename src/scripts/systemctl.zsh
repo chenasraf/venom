f8adef8c48907cf689ed905ffb94d829ef7e73fd
Chen Asraf <casraf@pm.me>
2024-08-05 20:53:55 +0000

    build: fix deploy scripts

--- src/scripts/systemctl.zsh
+++ src/scripts/systemctl.zsh
@@ -1,8 +1,13 @@
 #!/usr/bin/env zsh
 
-if [[ $# -eq 0 ]]; then
-  echo 'Usage: pnpm service <start|stop|restart|enable|disable>'
+if [[ $# -lt 2 ]]; then
+  echo 'Usage: pnpm service <start|stop|restart|enable|disable|logs>'
   return 1
 fi
 
-systemctl $1 venom-bot
+name=venom-$1
+if [[ "$2" == "logs" ]]; then
+  journalctl -n 30 -f -u $name.service
+else
+  systemctl $2 $name
+fi
