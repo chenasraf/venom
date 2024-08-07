f04407ee3ddeff67951f1e905896d0312dd93c6e
Chen Asraf <casraf@pm.me>
2024-08-07 15:00:17 +0300

    feat: improve settings command

--- src/commands/settings.command.ts
+++ src/commands/settings.command.ts
@@ -3,34 +3,43 @@ import { DEFAULT_COMMAND_PREFIX } from '@/env'
 import { logger } from '@/core/logger'
 import { getSetting, setSetting } from '@/lib/settings'
 
+const ERROR = `Invalid arguments. See \`${DEFAULT_COMMAND_PREFIX}help settings\``
+
 export default command({
   command: 'settings',
   aliases: ['set', 'setting'],
   description: 'Update settings for the bot (admin only)',
   examples: [
     `\`${DEFAULT_COMMAND_PREFIX}settings <key>\` - get setting value`,
-    `\`${DEFAULT_COMMAND_PREFIX}settings [-f] <key> <value>\` - set setting value. (add \`-f\` flag to force setting this key even if it doesn't exist yet).`,
+    `\`${DEFAULT_COMMAND_PREFIX}settings [-f] <key> <value>\` - set setting value. (add \`-f\` flag to force setting this key even if it doesn't exist yet). Strings should be wrapped in double quotes.`,
   ],
   adminOnly: true,
   async execute(message, args) {
-    let key: string,
+    let key: string | null = null,
       value: string | null = null,
       force = false
-    if (args.length === 1) {
-      key = args[0]
-    } else if (args.length === 2) {
-      key = args[0]
-      value = args[1]
-    } else if (args.length === 3) {
-      if (args[0] === '-f') {
-        force = true
-        key = args[1]
-        value = args[2]
-      } else {
-        return message.reply(`Invalid arguments. See \`${DEFAULT_COMMAND_PREFIX}help settings\``)
+    const _args = args.slice()
+    while (_args.length) {
+      switch (_args[0]) {
+        case '-f':
+          if (key || value || force) {
+            return message.reply(ERROR)
+          }
+          force = true
+          _args.shift()
+          break
+        default:
+          if (!key) {
+            key = _args.shift()!
+            if (_args.length) {
+              value = _args.shift()!
+              for (const _arg of _args) {
+                value += ' ' + _args.shift()!
+              }
+            }
+          }
+          break
       }
-    } else {
-      return message.reply(`Invalid arguments. See \`${DEFAULT_COMMAND_PREFIX}help settings\``)
     }
 
     if (!key) {
@@ -49,7 +58,7 @@ export default command({
       }
     }
 
-    await setSetting(key, value)
+    await setSetting(key, JSON.parse(value))
     logger.info(`Setting \`${key}\` updated to \`${value}\``)
     return message.reply(`Setting \`${key}\` updated to \`${value}\``)
   },
