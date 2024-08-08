43d7b9b706215d7b29e9e3075399a9c0ac3594a6
Chen Asraf <casraf@pm.me>
2024-08-08 14:33:27 +0300

    feat: update !see output

--- src/commands/see.command.ts
+++ src/commands/see.command.ts
@@ -1,14 +1,22 @@
 import { command } from '@/core/commands'
-import Discord from 'discord.js'
 
 export default command({
   command: 'see',
   aliases: ['s'],
   description: 'See your username and ID',
   examples: ['`!s`'],
-  async execute(message: Discord.Message): Promise<Discord.Message> {
+  async execute(message) {
     return message.author.send(
-      `Server: ${message.guild!.name}\nYour username: ${message.author.username}\nYour ID: ${message.author.id}`,
+      [
+        '**Server info:**',
+        `ID: ${message.guild!.id}`,
+        `Name: ${message.guild!.name}`,
+        '',
+        '**Your info:**',
+        `ID: ${message.author.id}`,
+        `Username: ${message.author.username}`,
+        `Nickname: ${message.member?.nickname ?? 'Unknown'}`,
+      ].join('\n'),
     )
   },
 })
